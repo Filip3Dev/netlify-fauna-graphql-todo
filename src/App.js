14106d7115243797e2e85395b0b3899f1dc53b17
Filipe Machado <filipwx7@gmail.com>
2021-02-12 11:37:22 -0300

    removendo unused things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useContext } from "react"
-import { Router, Link, navigate } from "@reach/router"
+import React, { useContext } from "react"
+import { Router, Link } from "@reach/router"
 
 import useNetlifyIdentity from "hooks/useNetlifyIdentity"
 import { UserCtx } from "contexts"
-import Footer from "./components/Footer"
+// import Footer from "./components/Footer"
 import Spinner from "./components/Spinner"
 import InputArea from "./components/InputArea"
-import TodoItem from "./components/TodoItem"
+// import TodoItem from "./components/TodoItem"
 import "./login.css"
 
 import { useQuery, useMutation } from "react-apollo-hooks"
@@ -43,15 +43,6 @@ function Login() {
   )
 }
 
-// const GET_COMMENTS = gql`
-//   query getcomments {
-//     allComments {
-//       name
-//       message
-//     }
-//   }
-// `
-
 function List(props) {
   return (
     <div>
@@ -60,88 +51,6 @@ function List(props) {
     </div>
   )
 }
-
-// function List(props) {
-//   const { data, error, loading } = useQuery(GET_COMMENTS)
-//   if (loading) {
-//     return <div>Loading...</div>
-//   }
-//   if (error) {
-//     return <div>Error! {error.message}</div>
-//   }
-
-//   const [state, setState] = useState(null)
-//   const { listId, uri } = props
-//   const pathFlag = props.path.split("/")[1] || "all"
-
-//   const shownTodos =
-//     state &&
-//     state.todos &&
-//     {
-//       all: state.todos,
-//       active: state.todos.filter(todo => !todo.data.completed),
-//       completed: state.todos.filter(todo => todo.data.completed)
-//     }[pathFlag]
-//   useEffect(
-//     () =>
-//       client &&
-//       void fetchList(listId)
-//         .then(setState)
-//         .catch(err => console.log({ err }) || setState({ err })),
-//     [client]
-//   )
-//   const [editing, setEditing] = useState(null)
-//   const edit = todo => () => setEditing(todo.ref)
-//   const onClearCompleted = () => load(clearCompleted(state.list, listId).then(setState))
-//   return (
-//     <div>
-//       {(isLoading || !state || !state.list) && <Spinner />}
-//       <div className="listNav">
-//         <label>List: {state && state.list.data.title}</label>
-//         <button onClick={() => navigate("/")}>back to all lists</button>
-//       </div>
-//       <ul className="todo-list">
-//         {state && state.err ? (
-//           <div>{JSON.stringify(state.err, null, 2)} </div>
-//         ) : (
-//           shownTodos &&
-//           shownTodos.map(todo => {
-//             const handle = fn => () => load(fn(todo, listId).then(setState))
-//             return (
-//               <TodoItem
-//                 key={todo.ref.value.id}
-//                 todo={todo.data}
-//                 onToggle={handle(toggle)}
-//                 onDestroy={handle(destroy)}
-//                 onEdit={edit(todo)}
-//                 editing={editing === todo.ref}
-//                 onSave={val => handle(save(val))()}
-//                 onCancel={console.log}
-//               />
-//             )
-//           })
-//         )}
-//       </ul>
-//       <InputArea
-//         onSubmit={title => load(addTodo(state.list, listId)(title).then(setState))}
-//         placeholder="Add a new item to your list."
-//       />
-
-//       {state && state.todos && (
-//         <Footer
-//           count={shownTodos.length}
-//           completedCount={state.todos.filter(todo => todo.data.completed).length}
-//           onClearCompleted={onClearCompleted}
-//           nowShowing={pathFlag}
-//           uri={uri
-//             .split("/")
-//             .slice(0, 3)
-//             .join("/")}
-//         />
-//       )}
-//     </div>
-//   )
-// }
 
 const GETALLLISTS = gql`
   query getcomments {
